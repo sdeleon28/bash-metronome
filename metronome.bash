64cc588aefdee9a8b541d8e72869a5451e0e355b
Santiago de Leon <sdeleon28@gmail.com>
2017-05-24 09:52:16 -0300

    Removed echos

--- metronome.bash
+++ metronome.bash
@@ -3,13 +3,10 @@ BPM="$1"
 ONE_MIN_MILLIS=60000
 INTERVAL_MILLIS=$(bc -l <<< "$ONE_MIN_MILLIS / $BPM")
 INTERVAL_SECS=$(bc -l <<< "$INTERVAL_MILLIS / 1000")
-echo "INTERVAL_MILLIS=${INTERVAL_MILLIS}"
-echo "INTERVAL_SECS=${INTERVAL_SECS}"
 
 while true
 do
   afplay ./click.mp3 &
-  echo "Waiting $INTERVAL_SECS"
   sleep "$INTERVAL_SECS"
 done
 
